2fd14bf0add9eeb2df122de38b217057540a68cb
sayantani-sen <74294422+sayantani-sen@users.noreply.github.com>
2021-01-16 19:07:24 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,10 +13,12 @@ function setup() {
   wall.shapeColor = color(80,80,80);
 }
 
-car.velocityX = speed;
-
 function draw() {
   background(200); 
+  
+  if(keyDown("space")){
+    car.velocityX = speed;
+  }
   
   if(wall.x - car.x < (car.width + wall.width)/2){
     car.velocityX = 0;
